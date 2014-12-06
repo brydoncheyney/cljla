32a42c880cc2d334822eac6eb17da80e68494ec1
Brydon Cheyney <brydon@remission.org.uk>
2014-12-06 19:14:18 +0000

    navigation elements

--- src/cljla/handler.clj
+++ src/cljla/handler.clj
@@ -8,11 +8,35 @@
 (defroutes app-routes
            (GET "/" [] "je$us loves amerika")
            (GET "/bootstrap" []
-                (view/page {:title    "je$us loves amerika"
-                            :header   "header"
-                            :articles [{:title "a" :content "aaaaa"}
-                                       {:title "b" :content "bbbbb"}
-                                       {:title "c" :content "ccccc"}]}))
+                (view/page {:title      "JE$US LOVES AMERIKA"
+                            :header     "Home"
+                            :navigation [{:text  "Home"
+                                          :title "News and updates"
+                                          :href  "/"}
+                                         {:text  "Bio"
+                                          :title "Coming soon..."
+                                          :href  "#"}
+                                         {:text  "Audio"
+                                          :title "JLA on Bandcamp"
+                                          :href  "http://jesuslovesamerika.bandcamp.com/"}
+                                         {:text  "Tour"
+                                          :title "Coming soon..."
+                                          :href  "#"}
+                                         {:text  "Gallery"
+                                          :title "Coming soon..."
+                                          :href  "#"}
+                                         {:text  "Contact"
+                                          :title "Email JLA"
+                                          :href  "mailto:info&#64;jesuslovesamerika.co.uk"}
+                                         {:text  "Links"
+                                          :title "Coming soon..."
+                                          :href  "#"}
+                                         {:text  "Shop"
+                                          :title "JLA on Bandcamp"
+                                          :href  "http://jesuslovesamerika.bandcamp.com/"}]
+                            :articles   [{:title "a" :content "aa<br />aaa"}
+                                         {:title "b" :content "bbbbb"}
+                                         {:title "c" :content "ccccc"}]}))
            (route/not-found "Not Found"))
 
 (def app
--- src/cljla/view.clj
+++ src/cljla/view.clj
@@ -5,16 +5,24 @@
 
 (def articles-template "public/templates/partials/articles.html")
 
+(html/defsnippet navigation-template bootstrap [:#navbar] [links]
+                 [:ul [:li (html/but html/first-of-type)]] nil
+                 [:ul [:li html/first-of-type]]
+                 (html/clone-for [{:keys [text title href]} links]
+                                 [:li] (if (not= href "/") (html/remove-class "active") identity)
+                                 [:li :a] (html/set-attr :href href :title title)
+                                 [:li :a] (html/content text)))
+
 (html/defsnippet articles-snippet articles-template [:#articles] [articles]
-                 ;[[:dl (html/but html/first-of-type)]] nil
                  {[:dt] [:dd]}
                  (html/clone-for [{:keys [content title]} articles]
                                  [:dt] (html/content title)
                                  [:dd] (html/content content)))
 
-(html/deftemplate index-page bootstrap [{:keys [title header articles]}]
-                  [:title] (html/content title)
-                  [:h1] (html/content header)
+(html/deftemplate index-page bootstrap [{:keys [title header navigation articles]}]
+                  [:title] (html/content (str title " | " header ))
+                  [:h1] (html/content title)
+                  [:#navbar] (html/content (navigation-template navigation))
                   [:#content] (html/content (articles-snippet articles)))
 
 (defn page
