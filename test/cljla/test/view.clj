332687baa6a3ccc9b3c419b4cc4c6066ba08afcf
Brydon Cheyney <brydon@remission.org.uk>
2015-03-16 22:30:05 +0000

    refactored view tests

--- test/cljla/test/view.clj
+++ test/cljla/test/view.clj
@@ -3,17 +3,20 @@
             [cljla.test.html-inspectors :refer [select select-single has-content?]]
             [cljla.view :refer :all]))
 
-(facts "about templates"
-       (fact "bootstrap template function should render content"
-             (let [content (page {:title    "foo"
-                                  :header   "bar"
-                                  :articles [{:title   "Article 1"
-                                              :content "text here"
-                                              :date    "2015-01-01"}
-                                             {:title   "Article 2"
-                                              :content "more text here"
-                                              :date    "2015-01-01"}]})]
-               (-> content (select-single [:title])) => (has-content? "foo | bar")
-               (-> content (select-single [:h1])) => (has-content? "foo")
-               (-> content (select [:dl#articles :dt]) count) => 2
+(facts "bootstrap template function should render content"
+       (let [content (page {:title    "foo"
+                            :header   "bar"
+                            :articles [{:title   "Article 1"
+                                        :content "text here"
+                                        :date    "2015-01-01"}
+                                       {:title   "Article 2"
+                                        :content "more text here"
+                                        :date    "2015-01-01"}]})]
+         (fact "page title"
+               (-> content (select-single [:title])) => (has-content? "foo | bar"))
+         (fact "h1"
+               (-> content (select-single [:h1])) => (has-content? "foo"))
+         (fact "articles"
+               (-> content (select [:dl#articles :dt]) count) => 2)
+         (fact "article title"
                (-> content (select [:dl#articles :dt]) first) => (has-content? "Article 1 - 2015-01-01"))))
