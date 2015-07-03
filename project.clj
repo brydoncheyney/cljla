65a1fd9bc1d40620cfba40830e8a062c30d5a0ae
Brydon Cheyney <brydon@remission.org.uk>
2015-07-03 22:52:49 +0100

    updated :profiles deps

--- project.clj
+++ project.clj
@@ -12,12 +12,12 @@
             :plugins [[lein-ring "0.8.13"]]
             :ring {:handler cljla.handler/app}
             :profiles {:dev     {:plugins      [[lein-midje "3.1.3"]
-                                                [lein-ancient "0.6.2"]
-                                                [lein-kibit "0.0.8"]
+                                                [lein-ancient "0.6.7"]
+                                                [lein-kibit "0.1.2"]
                                                 [jonase/eastwood "0.2.1"]]
                                  :dependencies [[javax.servlet/servlet-api "2.5"]
                                                 [ring-mock "0.1.5"]
-                                                [midje "1.6.3"]]}
+                                                [midje "1.7.0-beta1"]]}
                        :uberjar {:uberjar-name "cljla.jar"
                                  :main         cljla.core
                                  :aot          :all}})
