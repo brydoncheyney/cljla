59908f0e16bf559363c3291416288608662eee05
Brydon Cheyney <brydon@remission.org.uk>
2015-10-21 22:25:22 +0100

    formatted project.clj

--- project.clj
+++ project.clj
@@ -1,24 +1,24 @@
 (defproject cljla "0.1.0-SNAPSHOT"
-            :description "Je$us Loves Amerika web application"
-            :url "http://www.jesuslovesamerika.co.uk"
-            :min-lein-version "2.0.0"
-            :dependencies [[org.clojure/clojure "1.7.0"]
-                           [org.clojure/tools.logging "0.3.1"]
-                           [compojure "1.4.0"]
-                           [ring/ring-defaults "0.1.5"]
-                           [ring/ring-jetty-adapter "1.4.0"]
-                           [ring/ring-json "0.4.0"]
-                           [enlive "1.1.6"]
-                           [environ "1.0.1"]]
-            :plugins [[lein-ring "0.8.13"]]
-            :ring {:handler cljla.handler/app}
-            :profiles {:dev     {:plugins      [[lein-midje "3.1.3"]
-                                                [lein-ancient "0.6.7"]
-                                                [lein-kibit "0.1.2"]
-                                                [jonase/eastwood "0.2.1"]]
-                                 :dependencies [[javax.servlet/servlet-api "2.5"]
-                                                [ring-mock "0.1.5"]
-                                                [midje "1.7.0"]]}
-                       :uberjar {:uberjar-name "cljla.jar"
-                                 :main         cljla.core
-                                 :aot          :all}})
+  :description "Je$us Loves Amerika web application"
+  :url "http://www.jesuslovesamerika.co.uk"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [compojure "1.4.0"]
+                 [ring/ring-defaults "0.1.5"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [ring/ring-json "0.4.0"]
+                 [enlive "1.1.6"]
+                 [environ "1.0.1"]]
+  :plugins [[lein-ring "0.8.13"]]
+  :ring {:handler cljla.handler/app}
+  :profiles {:dev     {:plugins      [[lein-midje "3.1.3"]
+                                      [lein-ancient "0.6.7"]
+                                      [lein-kibit "0.1.2"]
+                                      [jonase/eastwood "0.2.1"]]
+                       :dependencies [[javax.servlet/servlet-api "2.5"]
+                                      [ring-mock "0.1.5"]
+                                      [midje "1.7.0"]]}
+             :uberjar {:uberjar-name "cljla.jar"
+                       :main         cljla.core
+                       :aot          :all}})
